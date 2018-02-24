5a8734660ae44e700ebf0b6c6b312f23d13c6457
luisgomezcaballero <luis.gomez.caballero@gmail.com>
2018-02-24 23:17:46 +0100

    Project upload

---
+++ sonar-demo/src/main/java/com/luisgomezcaballero/sonar_demo/Main.java
@@ -0,0 +1,17 @@
+package com.luisgomezcaballero.sonar_demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String parameter = "User";
+
+		if (parameter.equals("admin")) {
+			System.out.println("Admin user.");
+		} else {
+			System.out.println("Normal user.");
+		}
+
+	}
+
+}
